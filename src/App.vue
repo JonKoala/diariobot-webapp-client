897ca228c37fbd2a2003f05deb7a5c061581ba9a
JonKoala <victor.santana.lemos@gmail.com>
2017-11-09 17:41:04 -0200

    acessando api
    exibindo publicacoes randomicas

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
   <div id="app">
-    {{msg}}
+    <pre>{{msg}}</pre>
   </div>
 </template>
 
 <script>
+import getConfig from './common/appconfig'
+
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Testing 1 2 3'
+      msg: ''
     }
+  },
+  mounted() {
+    getConfig.then(config => {
+      Vue.axios.get(`${config['api']['url']}/publicacoes/rand`)
+        .then(response => {
+          this.msg = response.data.texto;
+        });
+    })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  margin: 10px;
+}
+
+pre {
+  margin: unset;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: justify;
   color: #2c3e50;
-  margin-top: 60px;
+  white-space: pre-wrap;
 }
 </style>
