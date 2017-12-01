ad290659fe43c0a0de80ef4806bc3fb1e1a42dd0
JonKoala <victor.santana.lemos@gmail.com>
2017-12-01 14:18:42 -0200

    adiciona logica para hostear o build do projeto

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var yaml = require('js-yaml');
+var fs = require('fs');
+var appconfig = yaml.safeLoad(fs.readFileSync('appconfig.yaml'));
+
+app.use(express.static(__dirname));
+
+app.get(/.+\.\w+$/, function(req, res) {
+  //requesting an unexisting file
+  res.status(404).send();
+});
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', { root : __dirname});
+});
+
+var port = appconfig.server.port;
+app.listen(port, () => {
+  console.log('Client up! Listening on ' + port + '...')
+})
--- src/common/appconfig.js
+++ src/common/appconfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import yaml from 'js-yaml'
 
 export default new Promise((res, rej) => {
-  return axios.get('./appconfig.yaml')
+  return axios.get('./appconfig.yml')
     .then(response => {
       res(yaml.load(response.data));
     });
