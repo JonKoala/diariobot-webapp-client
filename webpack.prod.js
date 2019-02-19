a177aa78774151b74ac235b9a52d99107353bd9c
JonKoala <victor.santana.lemos@gmail.com>
2019-02-19 20:06:01 -0300

    atualiza dependencias

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const merge = require('webpack-merge')
 const path = require('path')
 
-const common = require('./webpack.common.js')
+const common = require('./webpack.common')
 
 
 module.exports = merge(common, {
